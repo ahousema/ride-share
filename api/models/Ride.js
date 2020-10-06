0f6f59bd9a44bfcfdc227147d67e4083bd024c49
katelyn_averill@taylor.edu <kaverill@cse21706.cs.local>
2020-10-05 20:26:29 -0400

    added Ride.js

---
+++ api/models/Ride.js
@@ -0,0 +1,98 @@
+class Vehicle extends Model
+{
+	static get tableName()
+	{
+		return "vehicle";
+	}
+}
+class Driver extends Model
+{
+	static get tableName()
+	{
+		return "driver";
+	}
+}
+class User extends Model
+{
+	static get tableName()
+	{
+		return "user";
+	}
+}
+class Location extends Model
+{
+	static get tableName()
+	{
+		return "locat";
+	}
+}
+
+class Ride extends Model
+{
+	static get tableName()
+	{
+		return "ride";
+	}
+
+	static get relationMappings()
+	{
+		return
+		{
+			vehicle:
+			{
+				relation: Model.BelongsToOneRelation,
+				modelClass: Vehicle,
+				join:
+				{
+					from: 'ride.vehicleid',
+					to: 'vehicle.id'
+				}
+			}
+			driver:
+			{
+				relation: Model.ManyToManyRelation,
+				modelClass: Driver,
+				join:
+				{
+					from: 'ride.id',
+					through:
+					{
+						from: 'drivers.rideId',
+						to: 'drivers.driverId'
+					},
+					to: 'driver.id'
+				}
+			}
+			user:
+			{
+				relation: Model.ManyToManyRelation,
+				modelClass: User,
+				join:
+				{
+					from: 'ride.id',
+					through:
+					{
+						from: 'passenger.rideId',
+						to: 'passenger.passengerId'
+					},
+					to: 'user.id'
+				}
+			}
+			locat:
+			{
+				relation: Model.BelongsToOneRelation,
+				modelClass: Location,
+				join:
+				{
+					from: 'ride.fromLocationId',
+					to: 'location.id'
+				}, //if the second join thing doesn't work, i'm not sure how to fix this
+				join:
+				{
+					from: 'ride.toLocationId',
+					to: 'location.id'
+				}
+			}
+		};
+	}
+}
