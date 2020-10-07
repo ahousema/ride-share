3df8979dd74fb11a32b3b8da0d348440603f00d7
katelyn_averill@taylor.edu <kaverill@cse21712.cs.local>
2020-10-07 16:15:18 -0400

    did 1 small (but functional) test for user

--- api/test/test-users.js
+++ api/test/test-users.js
@@ -1,10 +1,14 @@
 //give access to knex and Model
-const { knex, Model } = require("/Documents/githubRepo/ride-share/api/db.js");
+const { knex, Model } = require("../db.js");
 
-User.query()
-	.withGraphFetched('drivers')
-	.where('id', 1)
-	.first()
-	.then(user => console.log(user))
-	.catch(error => console.log(error.message));
+//User.query()
+//	.withGraphFetched('drivers')
+//	.where('id', 1)
+//	.first()
+//	.then(user => console.log(user))
+//	.catch(error => console.log(error.message));
 
+knex
+	.select().table('User')
+	.then(result => console.log(result))
+	.then(() => knex.destroy());
