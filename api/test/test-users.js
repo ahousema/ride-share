efbc8b253ab87e3c493120b807eccb79f71ad3e4
katelyn_averill@taylor.edu <kaverill@cse21712.cs.local>
2020-10-07 16:51:24 -0400

    Included tests for adding and removing users

--- api/test/test-users.js
+++ api/test/test-users.js
@@ -8,7 +8,24 @@ const { knex, Model } = require("../db.js");
 //	.then(user => console.log(user))
 //	.catch(error => console.log(error.message));
 
+/*knex('User')
+    .insert({id: 5, firstName: 'Bo', lastName: 'Peep', email: 'shepherdgirl123@yahoo.com', password: 'w00dysg1rl', phone: '3216540987', isAdmin: false})
+	.then(result => console.log(result))
+	.then(() => knex.destroy());
+
 knex
 	.select().table('User')
 	.then(result => console.log(result))
 	.then(() => knex.destroy());
+
+knex('User')
+	.where('id', 5)
+	.del()
+	.then(result => console.log(result))
+	.then(() => knex.destroy());
+*/
+knex
+    .select().table('User')
+    .then(result => console.log(result))
+    .then(() => knex.destroy());
+
