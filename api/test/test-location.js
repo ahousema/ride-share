d1a5c43a51396c126258fbbc665be58a56189d7b
katelyn_averill@taylor.edu <kaverill@cse21712.cs.local>
2020-10-07 17:10:17 -0400

    started working on location test file

---
+++ api/test/test-location.js
@@ -0,0 +1,8 @@
+const { knex, Model } = require("../db.js");
+
+nex
+	.select('address', 'city', 'State.name') //Not sure how to change State.name to display as anything other than 'name'
+	.from('Location')
+	.innerJoin('State', 'Location.state', 'State.abbreviation')
+	.then(result => console.log(result))
+	.then(() => knex.destroy());
