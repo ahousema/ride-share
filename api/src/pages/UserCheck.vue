46c253295d14eead00ac14f521291206f55ad962
katelyn_averill@taylor.edu <kaverill@matthew.cs.local>
2020-12-01 03:37:48 -0500

    last commit?

--- api/src/pages/UserCheck.vue
+++ api/src/pages/UserCheck.vue
@@ -3,7 +3,7 @@
     <div>
       <h4 class="display-1">Check Rides</h4>
 	
-	<p class="body-1">Functionality coming soon...</p>
+<p class="body-1">Functionality coming soon...<br>This page is supposed to allow users to check rides for which they are already signed up.</p>
      
     </div>
   </v-container>
--- api/src/pages/UserJoin.vue
+++ api/src/pages/UserJoin.vue
@@ -3,7 +3,7 @@
     <div>
       <h4 class="display-1">Join Rides</h4>
 	
-	<p class="body-1">Functionality coming soon...</p>
+	<p class="body-1">Functionality coming soon...<br>This page is supposed to display available rides and allow users to join rides.</p>
      
     </div>
   </v-container>
